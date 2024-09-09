31bc6e60740d08e01b8bde7e8d562e3a555f7c40
DulankiMedonza <dulankimedonza@gmail.com>
2024-09-09 12:02:55 +0530

    app.js modifications

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -27,7 +27,7 @@ const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
-const getPlayerChoice = function(){
+const getPlayerChoice = () => {
     const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSOR}?`, '').toUpperCase();
     if (
         selection !== ROCK &&
@@ -35,12 +35,12 @@ const getPlayerChoice = function(){
         selection !== SCISSOR
     ) {
         alert(`Invalid choice!, We chose ${DEFAULT_USER_CHOICE} for you!`);
-        return DEFAULT_USER_CHOICE;
+        return;
     }
     return selection;
 };
 
-const getComputerChoice = function(){
+const getComputerChoice = () => {
     const randomValue = Math.random();
     if(randomValue < 0.34){
         return ROCK;
@@ -52,22 +52,26 @@ const getComputerChoice = function(){
     }
 };
 
-const getWinner = function (cChoice, pChoice){
-    if (cChoice === pChoice){
-        return RESULT_DRAW;
-    }
-    else if (
-    cChoice === ROCK && pChoice === PAPER || 
+const getWinner = (cChoice, pChoice = DEFAULT_USER_CHOICE) => 
+     cChoice === pChoice ? RESULT_DRAW : 
+    (cChoice === ROCK && pChoice === PAPER || 
     cChoice === PAPER && pChoice === SCISSOR ||
-    cChoice === SCISSOR && pChoice == ROCK){
-        return RESULT_PLAYER_WINS;
-    } else {
-        return RESULT_COMPUTER_WINS;
-    } 
-   
-};
+    cChoice === SCISSOR && pChoice == ROCK) ? RESULT_PLAYER_WINS : RESULT_COMPUTER_WINS;
 
-startGameBtn.addEventListener('click', function(){
+    // if (cChoice === pChoice){
+    //     return RESULT_DRAW;
+    // }
+    // else if (
+    // cChoice === ROCK && pChoice === PAPER || 
+    // cChoice === PAPER && pChoice === SCISSOR ||
+    // cChoice === SCISSOR && pChoice == ROCK){
+    //     return RESULT_PLAYER_WINS;
+    // } else {
+    //     return RESULT_COMPUTER_WINS;
+    // } 
+   
+
+startGameBtn.addEventListener('click', () => {
     if(gameIsRunning){
         return;
     }
@@ -76,7 +80,20 @@ startGameBtn.addEventListener('click', function(){
     const playerChoice = getPlayerChoice();
     console.log(playerChoice);
     const computerChoice = getComputerChoice();
-    const winner = getWinner(computerChoice, playerChoice);
-    console.log(winner);
-    
+    let winner;
+    if(playerChoice){
+        winner = getWinner(computerChoice, playerChoice);
+    } else {
+        winner = getWinner(computerChoice);
+    }
+    let message = `You Picked ${playerChoice || DEFAULT_USER_CHOICE}, computer picked ${computerChoice}, therefore you `;
+    if(winner === RESULT_DRAW){
+        message = message + `had a draw.`;
+    } else if(winner === RESULT_PLAYER_WINS){
+        message = message + `won.`;
+    } else {
+        message = message + `lost.`;
+    }
+    alert(message);
+    gameIsRunning = false;
 });
