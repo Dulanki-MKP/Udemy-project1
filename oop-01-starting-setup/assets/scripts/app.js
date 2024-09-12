58f2303312eb74382b31a0095afb0f95b1e925e6
DulankiMedonza <dulankimedonza@gmail.com>
2024-09-12 10:26:45 +0530

    get total and handle order now btn

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -20,9 +20,14 @@ class ElementAttribute {
 }
 
 class Component {
-  constructor(renderHookId) {
+  constructor(renderHookId, shouldRender = true) {
     this.hookId = renderHookId;
+    if(shouldRender) {
+      this.render();
+    }
   }
+
+  render() {}
 
   createRootElement(tag, cssClasses, attributes) {
     const rootElement = document.createElement(tag);
@@ -58,7 +63,12 @@ class ShoppingCart extends Component {
   }
 
   constructor(renderHookId) {
-    super(renderHookId);
+    super(renderHookId, false);
+    this.orderProducts = () => {
+      console.log('Ordering...');
+      console.log(this.items);
+    }
+    this.render();
   }
 
   addProduct(product) {
@@ -73,15 +83,18 @@ class ShoppingCart extends Component {
             <h2>Total : \$${0}</h2>
             <button>Order Now!</button>
         `;
+    const orderButton = cartEl.querySelector('button');
+    // orderButton.addEventListener('click', () => this.orderProducts());
+    orderButton.addEventListener('click', this.orderProducts);
     this.totalOutput = cartEl.querySelector('h2');
-    return cartEl;
   }
 }
 
 class ProductItem extends Component {
   constructor(product, renderHookId) {
-    super(renderHookId);
+    super(renderHookId, false);
     this.product = product;
+    this.render();
   }
 
   addToCart() {
@@ -107,42 +120,55 @@ class ProductItem extends Component {
 }
 
 class ProductList extends Component {
-  products = [
-    new Product(
-      "A Pillow",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6YaRJQgP2k4c_K5W8hLw9T8P0G_O-0sq-rQ&s",
-      "A soft pillow!",
-      19.99
-    ),
-    new Product(
-      "A Carpet",
-      "https://m.media-amazon.com/images/I/916yp2kvMcL._AC_UF894,1000_QL80_.jpg",
-      "A carpet which you might like or not!",
-      85.59
-    ),
-  ];
+  products = [];
 
   constructor(renderHookId) {
-    super(renderHookId);
+    super(renderHookId); 
+    this.fetchProducts();
+  }
+
+  fetchProducts() {
+    this.products =[
+      new Product(
+        "A Pillow",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6YaRJQgP2k4c_K5W8hLw9T8P0G_O-0sq-rQ&s",
+        "A soft pillow!",
+        19.99
+      ),
+      new Product(
+        "A Carpet",
+        "https://m.media-amazon.com/images/I/916yp2kvMcL._AC_UF894,1000_QL80_.jpg",
+        "A carpet which you might like or not!",
+        89.99
+      )
+    ];
+    this.renderProducts();
+  }
+
+  renderProducts() {
+    for (const prod of this.products) {
+      new ProductItem(prod, 'prod-list');
+    }
   }
 
   render() {
     this.createRootElement('ul', 'product-list', [
       new ElementAttribute('id', 'prod-list'),
     ]);
-    for (const prod of this.products) {
-      const productItem = new ProductItem(prod, 'prod-list');
-      productItem.render();
+    if(this.products && this.products.length > 0){
+      this.renderProducts();
     }
   }
 }
 
-class Shop {
+class Shop{
+  constructor(){
+    this.render();
+  }
+
   render() {
     this.cart = new ShoppingCart('app');
-    this.cart.render();
-    const productList = new ProductList('app');
-    productList.render();
+    new ProductList('app');
   }
 }
 
@@ -151,7 +177,6 @@ class App {
 
   static init() {
     const shop = new Shop();
-    shop.render();
     this.cart = shop.cart;
   }
 
